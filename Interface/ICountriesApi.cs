e783d49ce03413c61049e33b83cf73d467745cf5
Gatiskk <gatisk19@inbox.lv>
2023-03-01 11:56:22 +0200

    Adding pictures

---
+++ Interface/ICountriesApi.cs
@@ -0,0 +1,13 @@
+﻿using CountriesAPI.Models;
+using Refit;
+
+namespace CountriesAPI.Abstraction;
+
+public interface ICountriesApi
+{
+    [Get("/v2/regionalbloc/eu")]
+    Task<List<CountryModel>> GetCountries();
+
+    [Get("/v2/name/{name}")]
+    Task<List<CountryModel>> GetCountryByName(string name);
+}
